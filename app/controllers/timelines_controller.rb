109fd5006dc9dfeae6e542da83716be07a2fa803
Balint Erdi <balint.erdi@gmail.com>
2013-09-05 14:43:51 +0200

    Fix fetching user timelines

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -2,7 +2,13 @@ class TimelinesController < ApplicationController
   respond_to :json
 
   def home
-    response = api_get 'twitter/timelines/home.json'
+    response = api_get "twitter/timelines/home.json"
     respond_with response.body
   end
+
+  def show
+    response = api_get "twitter/timelines/#{params[:id]}.json"
+    respond_with response.body
+  end
+
 end
